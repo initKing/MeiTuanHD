3cab465e4108e1db73edb65a7f4e15eaa17256cf
CrazyHacker <king_to_King@163.com>
2016-08-01 16:27:46 +0800

    完成初步架构

---
+++ MeiTuanHD/MeiTuanHD/Classes/Home/MTHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTHomeViewController.h
+//  MeiTuanHD
+//
+//  Created by CrazyHacker on 16/8/1.
+//  Copyright © 2016年 CrazyHacker. All rights reserved.
+//
+
+#import "MTBaseViewController.h"
+
+@interface MTHomeViewController : MTBaseViewController
+
+@end
---
+++ MeiTuanHD/MeiTuanHD/Classes/Main/BaseView/MTBaseView.h
@@ -0,0 +1,13 @@
+//
+//  MTBaseView.h
+//  MeiTuanHD
+//
+//  Created by CrazyHacker on 16/8/1.
+//  Copyright © 2016年 CrazyHacker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTBaseView : UIView
+
+@end
