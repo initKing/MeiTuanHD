cc3027d630ec4046746ded11d462545b10aa5cd3
CrazyHacker <king_to_King@163.com>
2016-08-01 16:22:28 +0800

    初始化项目

---
+++ MeiTuanHD/MeiTuanHD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeiTuanHD
+//
+//  Created by CrazyHacker on 16/8/1.
+//  Copyright © 2016年 CrazyHacker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
